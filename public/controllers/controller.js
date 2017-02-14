b3a1ab34b16cdb8717f4259d2e64905688df840e
marlonli <jingyuanli94@gmail.com>
2017-02-14 18:51:03 -0500

    Add contacts

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,11 +1,24 @@
 var app = angular.module("myApp", []);
 
 app.controller("myCtrl", function($scope, $http) {
-	console.log("Hello World from controller!");
+    console.log("Hello World from controller!");
 
-$http.get('/contactlist').success(function(res) {
-	console.log("I got the data I requested");
-	$scope.contactlist = res;
-});
+    function refresh() {
+        $http.get('/contactlist').then(function(response) {
+            console.log("I got the data I requested");
+            $scope.contactlist = response.data;
+            console.log(response);
+        });
+    }
 
+    refresh();
+
+    $scope.addContact = function() {
+        console.log($scope.person);
+        $http.post('/addcontact', $scope.person).then(function(response) {
+            console.log(response);
+            refresh();
+            $scope.person = "";
+        });
+    };
 });
--- server.js
+++ server.js
@@ -1,36 +1,62 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
+
+// db.on('ready', function () {
+//     console.log('database connected')
+// });
 
 // app.get('/', function (req, res) {
 // 	response.send("Hello Word from server.js")
 // });
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));// /public/img will be shown as /img
+app.use(bodyParser.json());
+
+
+
+
 
 app.get('/contactlist', function(req, res) {
 	console.log("I recerved a GET request")
 
-	person1 = {
-		name: "Tim",
-		email: "tim@email.com",
-		number: "(111) 111-1111"
-	};
-
-	person2 = {
-		name: "Emily",
-		email: "emily@email.com",
-		number: "(222) 222-2222"
-	};
-
-	person3 = {
-		name: "John",
-		email: "John@email.com",
-		number: "(333) 333-3333"
-	};
-
-	var contactlist = [person1, person2, person3];
-	res.json(contactlist);
+	db.contactlist.find(function(err, docs) {
+		console.log(docs);
+		res.json(docs);
+	})
 });
+
+app.post('/addcontact', function(req, res){
+	console.log(req.body); //npm install body-parser
+	db.contactlist.insert(req.body, function(err, doc){ // doc represents the item we parsed and received
+		res.json(doc);	// send back to controller
+		console.log("res=============");
+		console.log(doc);
+	});
+});
+	// person1 = {
+	// 	name: "Tim",
+	// 	email: "tim@email.com",
+	// 	number: "(111) 111-1111"
+	// };
+
+	// person2 = {
+	// 	name: "Emily",
+	// 	email: "emily@email.com",
+	// 	number: "(222) 222-2222"
+	// };
+
+	// person3 = {
+	// 	name: "John",
+	// 	email: "John@email.com",
+	// 	number: "(333) 333-3333"
+	// };
+
+	// var contactlist = [person1, person2, person3];
+	// res.json(contactlist);
+
 
 app.listen(3000);
 console.log("Server running on port 3000");
